ba6d4c940d621391617d4d7b9a548ef6a81463b7
koouu <38750134+koouu@users.noreply.github.com>
2022-06-18 01:58:41 +0900

    プロジェクト ファイルを追加します。

---
+++ WANGameEngine/main.cpp
@@ -0,0 +1,77 @@
+#include <Windows.h>
+
+#ifdef _DEBUG
+#include<iostream>
+#include<tchar.h>
+#endif
+
+using namespace std;
+
+void DebugOutputFormatString(const char* format, ...) {
+#ifdef _DEBUG
+	va_list valist;
+	va_start(valist, format);
+	vprintf(format, valist);
+	va_end(valist);
+#endif
+}
+
+LRESULT WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+	if (msg == WM_DESTROY) {//ウィンドウが破棄されたら呼ばれます
+		PostQuitMessage(0);//OSに対して「もうこのアプリは終わるんや」と伝える
+		return 0;
+	}
+	return DefWindowProc(hwnd, msg, wparam, lparam);//規定の処理を行う
+}
+
+const unsigned int window_width = 1280;
+const unsigned int window_height = 720;
+
+#ifdef _DEBUG
+int main(){
+#else
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+#endif 
+	DebugOutputFormatString("string");
+	WNDCLASSEX w = {};
+	w.cbSize = sizeof(WNDCLASSEX);
+	w.lpfnWndProc = (WNDPROC)WindowProcedure;
+	w.lpszClassName = _T("WANGAMEENGINE");
+
+	w.hInstance = GetModuleHandle(0);
+	RegisterClassEx(&w);
+
+	RECT wrc = { 0,0, window_width, window_height };//ウィンドウサイズを決める
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);//ウィンドウのサイズはちょっと面倒なので関数を使って補正する
+	//ウィンドウオブジェクトの生成
+	HWND hwnd = CreateWindow(w.lpszClassName,//クラス名指定
+		_T("DX12テスト"),//タイトルバーの文字
+		WS_OVERLAPPEDWINDOW,//タイトルバーと境界線があるウィンドウです
+		CW_USEDEFAULT,//表示X座標はOSにお任せします
+		CW_USEDEFAULT,//表示Y座標はOSにお任せします
+		wrc.right - wrc.left,//ウィンドウ幅
+		wrc.bottom - wrc.top,//ウィンドウ高
+		nullptr,//親ウィンドウハンドル
+		nullptr,//メニューハンドル
+		w.hInstance,//呼び出しアプリケーションハンドル
+		nullptr);//追加パラメータ
+
+	ShowWindow(hwnd, SW_SHOW);
+
+	MSG msg = {};
+	while (1) {
+		if (PeekMessageW(&msg, nullptr, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessageW(&msg);
+		}
+		if (msg.message == WM_QUIT) {
+			break;
+		}
+	}
+	UnregisterClass(w.lpszClassName, w.hInstance);
+
+
+	return 0;
+
+}
+
