8e5f4b5dae4d0ba58904d3f13f0bb21345b1ede7
koouu <38750134+koouu@users.noreply.github.com>
2022-07-07 15:03:32 +0900

    shader読み込み

--- WANGameEngine/Graphics/GraphicsManager.cpp
+++ WANGameEngine/Graphics/GraphicsManager.cpp
@@ -2,6 +2,8 @@
 #include<tchar.h>
 #include<d3d12.h>
 #include<iostream>
+
+#include<d3dcompiler.h>
 #pragma comment(lib,"d3d12.lib")
 
 LRESULT WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
@@ -35,6 +37,7 @@ bool GraphicsManager::initializeGraphicsManager()
     Result &= initializeFance();
     Result &= initializeResource();
     Result &= initializeView();
+    Result &= initializeShader();
 
     return Result;
 }
@@ -293,6 +296,124 @@ bool GraphicsManager::initializeView()
     ibView.BufferLocation = idxBuff->GetGPUVirtualAddress();
     ibView.Format = DXGI_FORMAT_R16_UINT;
     ibView.SizeInBytes = sizeof(indices);
+
+    return true;
+}
+
+bool GraphicsManager::initializeShader()
+{
+    ID3DBlob* _vsBlob = nullptr;
+    ID3DBlob* _psBlob = nullptr;
+
+    ID3DBlob* errorBlob = nullptr;
+    HRESULT result = D3DCompileFromFile(L"Shader/BasicVertexShader.hlsl",
+        nullptr, D3D_COMPILE_STANDARD_FILE_INCLUDE,
+        "BasicVS", "vs_5_0",
+        D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION,
+        0, &_vsBlob, &errorBlob);
+    if (FAILED(result)) {
+        if (result == HRESULT_FROM_WIN32(ERROR_FILE_NOT_FOUND)) {
+            std::cout << "BasicVertexShaderファイルが見当たりません" << std::endl;
+        }
+        else {
+            std::string errstr;
+            errstr.resize(errorBlob->GetBufferSize());
+            std::copy_n((char*)errorBlob->GetBufferPointer(), errorBlob->GetBufferSize(), errstr.begin());
+            errstr += "\n";
+            std::cout << errstr.c_str() << std::endl;
+        }
+        exit(1);
+    }
+    result = D3DCompileFromFile(L"Shader/BasicPixelShader.hlsl",
+        nullptr, D3D_COMPILE_STANDARD_FILE_INCLUDE,
+        "BasicPS", "ps_5_0",
+        D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION,
+        0, &_psBlob, &errorBlob);
+    if (FAILED(result)) {
+        if (result == HRESULT_FROM_WIN32(ERROR_FILE_NOT_FOUND)) {
+            std::cout << "BasicPixelShaderファイルが見当たりません" << std::endl;
+        }
+        else {
+            std::string errstr;
+            errstr.resize(errorBlob->GetBufferSize());
+            std::copy_n((char*)errorBlob->GetBufferPointer(), errorBlob->GetBufferSize(), errstr.begin());
+            errstr += "\n";
+            std::cout << errstr.c_str() << std::endl;
+        }
+        exit(1);
+    }
+    D3D12_INPUT_ELEMENT_DESC inputLayout[] = {
+        { "POSITION",0,DXGI_FORMAT_R32G32B32_FLOAT,0,D3D12_APPEND_ALIGNED_ELEMENT,D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA,0 },
+    };
+
+    D3D12_GRAPHICS_PIPELINE_STATE_DESC gpipeline = {};
+    gpipeline.pRootSignature = nullptr;
+    gpipeline.VS.pShaderBytecode = _vsBlob->GetBufferPointer();
+    gpipeline.VS.BytecodeLength = _vsBlob->GetBufferSize();
+    gpipeline.PS.pShaderBytecode = _psBlob->GetBufferPointer();
+    gpipeline.PS.BytecodeLength = _psBlob->GetBufferSize();
+
+    gpipeline.SampleMask = D3D12_DEFAULT_SAMPLE_MASK;//中身は0xffffffff
+
+    //
+    gpipeline.BlendState.AlphaToCoverageEnable = false;
+    gpipeline.BlendState.IndependentBlendEnable = false;
+
+    D3D12_RENDER_TARGET_BLEND_DESC renderTargetBlendDesc = {};
+
+    //ひとまず加算や乗算やαブレンディングは使用しない
+    renderTargetBlendDesc.BlendEnable = false;
+    renderTargetBlendDesc.RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
+
+    //ひとまず論理演算は使用しない
+    renderTargetBlendDesc.LogicOpEnable = false;
+
+    gpipeline.BlendState.RenderTarget[0] = renderTargetBlendDesc;
+
+
+    gpipeline.RasterizerState.MultisampleEnable = false;//まだアンチェリは使わない
+    gpipeline.RasterizerState.CullMode = D3D12_CULL_MODE_NONE;//カリングしない
+    gpipeline.RasterizerState.FillMode = D3D12_FILL_MODE_SOLID;//中身を塗りつぶす
+    gpipeline.RasterizerState.DepthClipEnable = true;//深度方向のクリッピングは有効に
+
+    //残り
+    gpipeline.RasterizerState.FrontCounterClockwise = false;
+    gpipeline.RasterizerState.DepthBias = D3D12_DEFAULT_DEPTH_BIAS;
+    gpipeline.RasterizerState.DepthBiasClamp = D3D12_DEFAULT_DEPTH_BIAS_CLAMP;
+    gpipeline.RasterizerState.SlopeScaledDepthBias = D3D12_DEFAULT_SLOPE_SCALED_DEPTH_BIAS;
+    gpipeline.RasterizerState.AntialiasedLineEnable = false;
+    gpipeline.RasterizerState.ForcedSampleCount = 0;
+    gpipeline.RasterizerState.ConservativeRaster = D3D12_CONSERVATIVE_RASTERIZATION_MODE_OFF;
+
+
+    gpipeline.DepthStencilState.DepthEnable = false;
+    gpipeline.DepthStencilState.StencilEnable = false;
+
+    gpipeline.InputLayout.pInputElementDescs = inputLayout;//レイアウト先頭アドレス
+    gpipeline.InputLayout.NumElements = _countof(inputLayout);//レイアウト配列数
+
+    gpipeline.IBStripCutValue = D3D12_INDEX_BUFFER_STRIP_CUT_VALUE_DISABLED;//ストリップ時のカットなし
+    gpipeline.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;//三角形で構成
+
+    gpipeline.NumRenderTargets = 1;//今は１つのみ
+    gpipeline.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM;//0～1に正規化されたRGBA
+
+    gpipeline.SampleDesc.Count = 1;//サンプリングは1ピクセルにつき１
+    gpipeline.SampleDesc.Quality = 0;//クオリティは最低
+
+    ID3D12RootSignature* rootsignature = nullptr;
+
+    D3D12_ROOT_SIGNATURE_DESC rootSignatureDesc = {};
+    rootSignatureDesc.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
+
+    ID3DBlob* rootSigBlob = nullptr;
+    D3D12SerializeRootSignature(&rootSignatureDesc, D3D_ROOT_SIGNATURE_VERSION_1_0, &rootSigBlob, &errorBlob);
+    device->CreateRootSignature(0, rootSigBlob->GetBufferPointer(), rootSigBlob->GetBufferSize(), IID_PPV_ARGS(&rootsignature));
+    rootSigBlob->Release();
+
+    gpipeline.pRootSignature = rootsignature;
+    
+    device->CreateGraphicsPipelineState(&gpipeline, IID_PPV_ARGS(&pipelinestate));
 
     return true;
 }
